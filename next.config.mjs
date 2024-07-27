140e9e51496d35d0b798f38f19a24ffd1678bb17
developerhost <kanonhara0609@gmail.com>
2024-07-27 17:36:29 +0900

    feat: shadcn/uiをインストール

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // styled-componentsを有効化
-  compiler: {
-    styledComponents: true,
-  },
 };
 
 export default nextConfig;
